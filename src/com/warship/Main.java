e4606a87c887afd797d20ebd4839af86c43ebc71
dovahkiin <petar.ilic1337@gmail.com>
2019-12-12 16:30:44 +0100

    main testing

--- src/com/warship/Main.java
+++ src/com/warship/Main.java
@@ -3,9 +3,23 @@ package com.warship;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("This shit compiles");
+	    System.out.println("This compiles");
 
+        SmolShip smol = new SmolShip();
+        BigShip bigChungus = new BigShip();
+        CommandShip fleetComander = new CommandShip();
+        BigCargo carGO = new BigCargo();
 
+        fleetComander.addToFleet(smol);
+
+        bigChungus.attack(smol);
+        bigChungus.attack(fleetComander);
+        bigChungus.attack(fleetComander);
+        bigChungus.attack(carGO);
+
+        System.out.println(smol.getHp());
+        System.out.println(fleetComander.getHp());
+        System.out.println(carGO.getHp());
 
     }
 }
